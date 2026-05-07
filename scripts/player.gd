4994b2ab251ed8514f488f3852c3200066834a04
BilguunBttgs <bobbebe40@gmail.com>
2026-05-07 14:13:02 +0800

    added running jumping animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 130.0
@@ -6,17 +7,34 @@ const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
+	
+	var direction := Input.get_axis("move_left", "move_right")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite.play("jump")
+	else:
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	
+	if direction >0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
